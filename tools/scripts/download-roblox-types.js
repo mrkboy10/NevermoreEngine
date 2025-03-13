585ed1ac363badae216f509d8324f55c90f5b98e
James Onnen <jonnen@roblox.com>
2025-03-13 12:31:24 -0700

    ci: Add luau-lsp type checking github action and integration

---
+++ tools/scripts/download-roblox-types.js
@@ -0,0 +1,11 @@
+var https = require('https');
+var fs = require('fs');
+
+function download(filename, url) {
+  var file = fs.createWriteStream(filename);
+  var request = https.get(url, function(response) {
+    response.pipe(file);
+  });
+}
+
+download('globalTypes.d.lua', 'https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua');
